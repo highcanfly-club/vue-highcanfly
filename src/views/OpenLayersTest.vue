646d779d5ca1ecd8413e7c2902abfa7c60c38d21
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-20 10:47:08 +0100

    Integrate OpenLayers in Demo.vue

--- src/views/OpenLayersTest.vue
+++ src/views/OpenLayersTest.vue
@@ -1,4 +1,43 @@
 <template>
+  <div>
+    <navbar />
+    <main class="profile-page">
+      <section class="relative block h-500-px">
+        <div
+          class="absolute top-0 w-full h-full bg-center bg-cover"
+          v-bind:style="{ background: backgroundImageURL }"
+        >
+          <span
+            id="blackOverlay"
+            class="w-full h-full absolute opacity-50 bg-grey"
+          ></span>
+        </div>
+        <div
+          class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+          style="transform: translateZ(0)"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              class="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+      </section>
+      <section class="relative py-16 bg-blueGray-200">
+        <div class="container mx-auto px-4">
+          <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
+          >
+<div class="px-6 py-6">
 <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px">
 
     <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" 
@@ -9,12 +48,32 @@
     </ol-tile-layer>
     
 </ol-map>
+</div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer-component />
+  </div>
 </template>
 <script>
+import Navbar from "@/components/Navbars/Navbar.vue";
+import FooterComponent from "@/components/Footers/Footer.vue";
+import backgroundImageAsset from "@/assets/img/blancnezhugues-101.jpg";
 import {
     ref
 } from 'vue'
+
 export default {
+  data() {
+    return {
+      backgroundImageURL: 'url("'+backgroundImageAsset+'")',
+    };
+  },
+  components: {
+    Navbar,
+    FooterComponent,
+  },
     setup() {
         const center = ref([40, 40])
         const projection = ref('EPSG:4326')
@@ -27,5 +86,5 @@ export default {
             rotation
         }
     },
-}
+};
 </script>
