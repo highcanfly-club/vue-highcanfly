060c21a3899f1bcb5d21202e4619c716193669f0
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-23 18:34:24 +0100

    Load asynchronusly Google Map if needed

--- src/components/Maps/MapFlersComponent.vue
+++ src/components/Maps/MapFlersComponent.vue
@@ -9,81 +9,105 @@
 <script>
 export default {
   mounted() {
-    let google = window.google;
-    let map = document.getElementById("map-canvas");
-    let lat = map.getAttribute("data-lat");
-    let lng = map.getAttribute("data-lng");
-
-    const myLatlng = new google.maps.LatLng(lat, lng);
-    const mapOptions = {
-      zoom: 15,
-      scrollwheel: false,
-      center: myLatlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      styles: [
-        {
-          featureType: "administrative",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#444444" }],
-        },
-        {
-          featureType: "landscape",
-          elementType: "all",
-          stylers: [{ color: "#f2f2f2" }],
-        },
-        {
-          featureType: "poi",
-          elementType: "all",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "road",
-          elementType: "all",
-          stylers: [{ saturation: -100 }, { lightness: 45 }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "all",
-          stylers: [{ visibility: "simplified" }],
-        },
-        {
-          featureType: "road.arterial",
-          elementType: "labels.icon",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "transit",
-          elementType: "all",
-          stylers: [{ visibility: "off" }],
-        },
-        {
-          featureType: "water",
-          elementType: "all",
-          stylers: [{ color: "#5e72e4" }, { visibility: "on" }],
-        },
-      ],
-    };
-
-    map = new google.maps.Map(map, mapOptions);
-
-    const marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: "Hello World!",
-    });
-
-    const contentString =
-      '<div class="info-window-content"><h2>High Can Fly</h2>' +
-      "<p>119 rue Marceau Martin, 59128 Flers</p></div>";
-
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
-
-    google.maps.event.addListener(marker, "click", function () {
-      infowindow.open(map, marker);
-    });
+    this.checkAndAttachMapScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAvVL6SovqBi_iz8aew6I4zheqcr82uyv4',this.mapActivation);
   },
+  methods: {
+    checkAndAttachMapScript(url,callback) {
+  var script = document.createElement( "script" )
+  script.type = "text/javascript";
+  if(script.readyState) {  // only required for IE <9
+    script.onreadystatechange = function() {
+      if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  //Others
+    script.onload = function() {
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.getElementsByTagName( "head" )[0].appendChild( script );
+    },
+    mapActivation() {
+      let google = window.google;
+      let map = document.getElementById("map-canvas");
+      let lat = map.getAttribute("data-lat");
+      let lng = map.getAttribute("data-lng");
+
+      const myLatlng = new google.maps.LatLng(lat, lng);
+      const mapOptions = {
+        zoom: 12,
+        scrollwheel: false,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+          {
+            featureType: "administrative",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#444444" }],
+          },
+          {
+            featureType: "landscape",
+            elementType: "all",
+            stylers: [{ color: "#f2f2f2" }],
+          },
+          {
+            featureType: "poi",
+            elementType: "all",
+            stylers: [{ visibility: "off" }],
+          },
+          {
+            featureType: "road",
+            elementType: "all",
+            stylers: [{ saturation: -100 }, { lightness: 45 }],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "all",
+            stylers: [{ visibility: "simplified" }],
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.icon",
+            stylers: [{ visibility: "off" }],
+          },
+          {
+            featureType: "transit",
+            elementType: "all",
+            stylers: [{ visibility: "off" }],
+          },
+          {
+            featureType: "water",
+            elementType: "all",
+            stylers: [{ color: "#5e72e4" }, { visibility: "on" }],
+          },
+        ],
+      };
+
+      map = new google.maps.Map(map, mapOptions);
+
+      const marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "Hello World!",
+      });
+
+      const contentString =
+        '<div class="info-window-content"><h2>Vue Notus</h2>' +
+        "<p>A beautiful UI Kit and Admin for Tailwind CSS. It is Free and Open Source.</p></div>";
+
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+      google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+      });
+    }
+  }
 };
 </script>
