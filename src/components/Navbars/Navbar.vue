2eeb96bb524bc3120cfe1c497cb2576bb3dc058f
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-23 13:45:41 +0100

    Bug no licence on mobile

--- src/components/Navbars/Navbar.vue
+++ src/components/Navbars/Navbar.vue
@@ -2,9 +2,7 @@
   <nav
     class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
   >
-    <div
-      class="container px-4 mx-auto flex flex-wrap items-start justify-between"
-    >
+    <div class="container px-4 mx-auto flex flex-wrap items-start justify-between">
       <div
         class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
       >
@@ -12,16 +10,16 @@
           class="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
           to="/"
         >
-          High Can Fly <img src="@/assets/img/logo_high_can_fly.svg" />
+          High Can Fly
+          <img src="@/assets/img/logo_high_can_fly.svg" />
         </router-link>
-
       </div>
       <div
-        class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none "
+        class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none"
         :class="[navbarOpen ? 'block rounded shadow-lg' : 'hidden']"
         id="example-navbar-warning"
       >
-        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto ">
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
             <PagesDropdown />
           </li>
@@ -31,9 +29,7 @@
               href="https://www.instagram.com/highcanfly_parapente/"
               target="_blank"
             >
-              <i
-                class="lg:text-slate-200 text-slate-400 fab fa-instagram text-lg leading-lg"
-              />
+              <i class="lg:text-slate-200 text-slate-400 fab fa-instagram text-lg leading-lg" />
               <span class="lg:hidden inline-block ml-2">Share</span>
             </a>
           </li>
@@ -44,9 +40,7 @@
               href="https://twitter.com/HighCanFlyClub"
               target="_blank"
             >
-              <i
-                class="lg:text-slate-200 text-slate-400 fab fa-twitter text-lg leading-lg"
-              />
+              <i class="lg:text-slate-200 text-slate-400 fab fa-twitter text-lg leading-lg" />
               <span class="lg:hidden inline-block ml-2">Tweet</span>
             </a>
           </li>
@@ -57,9 +51,7 @@
               href="https://github.com/eltorio/vue-highcanfly/tree/highcanfly?ref=vn-auth-navbar"
               target="_blank"
             >
-              <i
-                class="lg:text-slate-200 text-slate-400 fab fa-github text-lg leading-lg"
-              />
+              <i class="lg:text-slate-200 text-slate-400 fab fa-github text-lg leading-lg" />
               <span class="lg:hidden inline-block ml-2">Star</span>
             </a>
           </li>
@@ -74,6 +66,15 @@
             </button>
           </li>
         </ul>
+      </div>
+      <div class="sm:hidden flex items-center absolute top-4 right-3">
+        <button
+          class="bg-white text-slate-700 active:bg-slate-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+          type="button"
+          onclick="location.href='https://intranet.ffvl.fr/ffvl_licenceonline/pre_rempli/NEW/14172'"
+        >
+          <i class="fas fa-arrow-alt-circle-down"></i> Prenez votre licence
+        </button>
       </div>
     </div>
   </nav>
