73dda92b30092f09badabcbfb7c769aaef879661
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-23 10:23:04 +0100

    Favicons

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
     devtool: 'source-map',
   },
   chainWebpack(config) {
+    config.resolve.alias.set( 'vue', path.resolve('./node_modules/vue')); //if using yarn rather than npm
     //config.resolve.alias.set( '...', path.resolve('./node_modules/tailwindcss')) //bug in tailwindcss ???
     }
 };
