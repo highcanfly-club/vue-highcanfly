4620a2dc3c53960dfb344b3ea7e2973e84635205
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-24 08:52:22 +0100

    Adds Windy card

---
+++ src/components/Cards/CardWindy.vue
@@ -0,0 +1,14 @@
+<template>
+ <div
+  class="relative flex flex-col min-w-0 break-words w-full shadow-lg rounded bg-slate-700"
+ >
+  <div class="p-0.5 h-full">
+   <iframe
+    class="w-full h-full rounded"
+    src="https://embed.windy.com/embed2.html?lat=50.475&lon=2.390&detailLat=50.419&detailLon=2.903&width=650&height=450&zoom=8&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=&calendar=24&pressure=true&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=default&radarRange=-1"
+   ></iframe>
+  </div>
+ </div>
+</template>
+
+<script></script>
