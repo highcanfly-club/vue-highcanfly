f8dc2ed994e3bf5e2a0d622daa3d94c3f329f0b8
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-04 19:33:21 +0100

    Working z-index

--- src/components/Navbars/NavbarWhite.vue
+++ src/components/Navbars/NavbarWhite.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+    class="top-0 fixed z-40 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
   >
     <div
       class="container px-4 mx-auto flex flex-wrap items-center justify-between"
