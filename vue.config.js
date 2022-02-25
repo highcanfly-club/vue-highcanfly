90b06d27c8f043155cf0651042baa7e2a98b4728
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-25 08:11:46 +0100

    First meteo in /index-new

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,7 @@ var path = require('path');
 module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
-    devtool: process.env.CF_PAGES === '1' ? false : 'source-map',//'eval-source-map',
+    devtool: process.env.CF_PAGES === '1' ? false : 'eval',//'source-map',//'eval-source-map',
     mode: 'production',
     resolve: {
       fallback: {
