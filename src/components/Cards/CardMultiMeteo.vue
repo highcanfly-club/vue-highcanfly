8f1c1ac58e05147535ac40684f9db93070950e24
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-26 12:48:24 +0100

    Sitemap updated with places

--- src/components/Cards/CardMultiMeteo.vue
+++ src/components/Cards/CardMultiMeteo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="flex justify-center">
     <div
       class="flex flex-wrap sm:flex-nowrap sm:flex-row overflow-x-auto items-center"
     >
@@ -26,57 +26,9 @@
 </template>
 <script>
 import LazyObserver from "@/components/Utilities/LazyObserver.vue";
-//import { defineAsyncComponent } from "vue";
 import CardMeteo from "@/components/Cards/CardMeteo.vue";
-const places = [
-  {
-    lat: 50.416924,
-    lon: 2.513619,
-    slug: "la-comte",
-    name: "La Comté",
-    fly: {
-      sectors: [
-        [-1, 15],
-        [270, 360],
-      ],
-      wind: [0, 5.55],
-    },
-  },
-  {
-    lat: 50.43358,
-    lon: 2.585847,
-    slug: "ohlain",
-    name: "Parc d'Ohlain",
-    fly: { sectors: [[180, 225]], wind: [0, 5.55] },
-  },
-  {
-    lat: 50.679484,
-    lon: 1.567162,
-    slug: "equihen",
-    name: "Équihen-Plage",
-    fly: { sectors: [[250, 290]], wind: [0, 6.11] },
-  },
-  {
-    lat: 50.401719,
-    lon: 2.92927,
-    slug: "aquaterra",
-    name: "Gonflage au Parc des Îles",
-    fly: { sectors: [[-1, 361]], wind: [0, 4] },
-  },
-  {
-    lat: 50.443306,
-    lon: 2.778216,
-    slug: "11-19",
-    name: "⚠ Terrils jumeaux ⚠",
-    fly: {
-      sectors: [
-        [-1, 30],
-        [260, 360],
-      ],
-      wind: [0, 1.666],
-    },
-  },
-];
+const places = require("@/places.json");
+
 export default {
   data() {
     const slug = this.$route.params.slug
