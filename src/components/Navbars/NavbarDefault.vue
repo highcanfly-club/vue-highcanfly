2637797efc19a3f107f979236f3412818a174626
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-27 17:48:46 +0100

    new navigation on small screens

--- src/components/Navbars/NavbarDefault.vue
+++ src/components/Navbars/NavbarDefault.vue
@@ -2,7 +2,9 @@
   <nav
     class="top-0 absolute z-40 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg"
   >
-    <div class="container px-4 mx-auto flex flex-wrap items-start justify-between">
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-start justify-between"
+    >
       <div
         class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
       >
@@ -14,15 +16,26 @@
           High Can Fly
           <img src="@/assets/img/logo_high_can_fly.svg" />
         </router-link>
+        <button
+          :class="color"
+          class="self-start cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          v-on:click="setNavbarOpen"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
       </div>
       <div
-        class="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none"
-        :class="[navbarOpen ? 'block rounded shadow-lg' : 'hidden']"
-        id="example-navbar-warning"
+        class="lg:flex lg:items-center lg:bg-opacity-0 lg:shadow-none"
+        :class="[
+          navbarOpen
+            ? 'absolute right-3 block rounded shadow-lg mt-8 z-50 bg-white/50 pr-3'
+            : 'hidden',
+        ]"
       >
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
-            <PagesDropdown :color="color" :colorhover="colorhover"/>
+            <PagesDropdown :color="color" :colorhover="colorhover" />
           </li>
           <li class="flex items-center">
             <a
@@ -30,8 +43,13 @@
               href="https://www.instagram.com/highcanfly_parapente/"
               target="_blank"
             >
-              <i :class="iconscolor" class=" fab fa-instagram text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Share</span>
+              <i
+                :class="iconscolor"
+                class="fab fa-instagram text-lg leading-lg"
+              />
+              <span :class="color" class="lg:hidden inline-block ml-2"
+                >instagram</span
+              >
             </a>
           </li>
 
@@ -41,8 +59,11 @@
               href="https://twitter.com/HighCanFlyClub"
               target="_blank"
             >
-              <i :class="iconscolor" class=" fab fa-twitter text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Tweet</span>
+              <i
+                :class="iconscolor"
+                class="fab fa-twitter text-lg leading-lg"
+              />
+              <span :class="color" class="lg:hidden inline-block ml-2">tweet</span>
             </a>
           </li>
 
@@ -52,8 +73,8 @@
               href="https://github.com/eltorio/vue-highcanfly/tree/highcanfly?ref=vn-auth-navbar"
               target="_blank"
             >
-              <i :class="iconscolor" class=" fab fa-github text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Star</span>
+              <i :class="iconscolor" class="fab fa-github text-lg leading-lg" />
+              <span :class="color" class="lg:hidden inline-block ml-2">github</span>
             </a>
           </li>
 
@@ -69,7 +90,7 @@
           </li>
         </ul>
       </div>
-      <div class="sm:hidden flex items-center absolute top-4 right-3">
+      <!-- <div class="sm:hidden flex items-center absolute top-4 right-3">
         <button
           :class="buttoncolor"
           class="text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
@@ -78,7 +99,7 @@
         >
           <i class="fas fa-arrow-alt-circle-down"></i> Prenez votre licence
         </button>
-      </div>
+      </div> -->
     </div>
   </nav>
 </template>
