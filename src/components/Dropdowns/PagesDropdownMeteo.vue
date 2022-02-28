51e937549f4b8d41a6ab72fd129be9a12c0f477d
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-02-28 07:06:09 +0100

    mouseleave on menu

--- src/components/Dropdowns/PagesDropdownMeteo.vue
+++ src/components/Dropdowns/PagesDropdownMeteo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div @mouseleave="toggleDropdown($event)">
     <a
       class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-slate-700"
       href="#meteo"
