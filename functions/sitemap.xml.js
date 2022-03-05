788dfe05ca1ddfabaabc90fdddbcb2c656573210
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-03-05 13:58:20 +0100

    workaround test for cloudflare

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -2,6 +2,7 @@ import { getResponsePaths } from "@/sitemapHelper.js";
 
 
 export async function onRequestGet(context) { //eslint-disable-line
+    global.fetchDate = Date.now();
     let originUrl = (new URL(context.request.url)).origin;
     let globPaths = await getResponsePaths(originUrl);
     return new Response(globPaths.xml, {
