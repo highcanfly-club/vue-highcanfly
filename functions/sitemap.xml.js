774571efce00008f4189441cd0e359ff884eb7e1
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-03-05 14:29:50 +0100

    workaround continue for cloudflare

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -2,7 +2,7 @@ import { getResponsePaths } from "@/sitemapHelper.js";
 
 
 export async function onRequestGet(context) { //eslint-disable-line
-    global.fetchDate = Date.now();
+    globalThis.fetchDate = Date.now();//eslint-disable-line
     let originUrl = (new URL(context.request.url)).origin;
     let globPaths = await getResponsePaths(originUrl);
     return new Response(globPaths.xml, {
