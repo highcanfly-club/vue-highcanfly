38336946a69199b28818d95673f90dcf4dfd4bb0
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-03-12 18:46:20 +0100

    blockquote support in sanity

--- src/components/Cards/CardSinglePost.vue
+++ src/components/Cards/CardSinglePost.vue
@@ -86,6 +86,8 @@ import { sanityReplaceReferences } from "@/plugins/SanityReferenceWalker";
 import imageUrlBuilder from "@sanity/image-url";
 import SanityGallerySerializer from "@/components/Utilities/SanityGallerySerializer.vue";
 import SanityLazyImgSerializer from "@/components/Utilities/SanityLazyImgSerializer.vue";
+import SanityBlockquoteSerializer from "@/components/Utilities/SanityBlockquoteSerializer.vue";
+
 import * as basiclightbox from "basiclightbox";
 const client = sanityClient({
   projectId: process.env.VUE_APP_SANITY_PROJECT_ID,
@@ -127,10 +129,7 @@ const postSerializers = {
     },
   },
   styles: {
-    blockquote: (props, children) => {
-      //eslint-disable-line no-unused-vars
-      return h("blockquote", {}, children.slots.default()[0].children);
-    },
+    blockquote: SanityBlockquoteSerializer,
   },
 };
 
---
+++ src/components/Utilities/SanityBlockquoteSerializer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <blockquote
+      className="relative p-4 text-lg italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500 quote"
+    >
+      <div
+        className="absolute -left-0.5 top-1 text-5xl font-serif stylistic-quote-mark"
+      >
+        &ldquo;
+      </div>
+      <cite v-html="blockquote"> </cite>
+      <div
+        className="absolute right-1.5 -bottom-4 text-5xl font-serif stylistic-quote-mark"
+      >
+        &rdquo;
+      </div>
+    </blockquote>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicCard",
+  data() {
+    let blockquote = "";
+    this.$slots.default().forEach((element) => {
+      switch (element.type) {
+        case "strong":
+          blockquote += `<b>${element.children}</b>`;
+          break;
+        case "em":
+          blockquote += `<em>${element.children}</em>`;
+          break;
+        case "mark":
+          blockquote += `<mark>${element.children}</mark>`;
+          break;
+        default:
+          blockquote += element.children;
+      }
+    });
+    return {
+      blockquote: blockquote,
+    };
+  },
+  methods: {},
+  components: {},
+};
+</script>
