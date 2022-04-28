fb934b29ddaedbe4f37056dff3d430580e6fb284
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-04-28 11:36:53 +0200

    Left debuggind code

--- src/components/Utilities/ComponentMinSanityBlog.vue
+++ src/components/Utilities/ComponentMinSanityBlog.vue
@@ -43,7 +43,6 @@ export default {
     };
   },
   created() {
-    console.log('created');
     const { initializationCompleted, user, isAuthenticated } = useAuth0();
     initializationCompleted().then(() => {
       console.log('initializationCompleted()');
