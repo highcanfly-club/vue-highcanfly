389bdbe664498993d1cfa0219b8897ae85c6b14f
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-04-28 10:35:44 +0200

    better class selection for mangling

--- vue.config.js
+++ vue.config.js
@@ -94,7 +94,7 @@ if ((process.env.CF_PAGES === '1') && (process.env.__DEBUG__ !== '1')) {
     glyphs: [],
     allowedFilesRegex: /^fa[srltdb]*-/, // RegExp to only target specific fonts by their names
     skippedFilesRegex: null, // RegExp to skip specific fonts by their names
-    textRegex: /\.(js|css|html|vue)$/,  // RegExp for searching text reference
+    textRegex: /\.(ts|js|css|html|vue)$/,  // RegExp for searching text reference
     webpackCompilationHook: 'compilation', // Webpack compilation hook (for example PurgeCss webpack plugin use 'compilation' )
   });
   webpackPlugins.push(fontMinPlugin);
@@ -104,11 +104,12 @@ if ((process.env.CF_PAGES === '1') && (process.env.__DEBUG__ !== '1')) {
   const MangleCssClassPlugin = require('mangle-css-class-webpack-plugin');
   const myManglePlugin = new MangleCssClassPlugin({
     //be carrefull to not mangle javascript code ie: tailwindcss 'static' is a javascript reserved word !
-    classNameRegExp: '\\b([-]*left|[-]*top|[-]*right|[-]*bottom)-[p0-9]+|\\b(bg|[-]*p[xylrbt]*|[-]*m[xylrbt]*|w|[-]*z|h|justify|overflow|border|max-h|min-h|max-w|min-w|flex|text|font|prose|inline|line|rounded|from|to|via|contrast|brightness|leading|items|place|align|backdrop|shadow|duration|whitespace|self|cursor|transition|outline)-[a-z0-9_-]+\\b|\\brelative\\b|\\bshadow\\b|\\bflex\\b|\\brounded\\b|\\bborder\\b|\\bunderline\\b', //\\babsolute\\b|\\fixed\\b| fixed and absolute cannot be mangle with PopperJS because PopperJS us 'absolute' and 'fixed' as strategy name
+    //classNameRegExp: '\\b([-]*left|[-]*top|[-]*right|[-]*bottom)-[p0-9]+|\\b(bg|[-]*p[xylrbt]*|[-]*m[xylrbt]*|w|[-]*z|h|justify|overflow|border|max-h|min-h|max-w|min-w|flex|text|font|prose|inline|line|rounded|from|to|via|contrast|brightness|leading|items|place|align|backdrop|shadow|duration|whitespace|self|cursor|transition|outline)-[a-z0-9_-]+\\b|\\brelative\\b|\\bshadow\\b|\\bflex\\b|\\brounded\\b|\\bborder\\b|\\bunderline\\b', //\\babsolute\\b|\\fixed\\b| fixed and absolute cannot be mangle with PopperJS because PopperJS us 'absolute' and 'fixed' as strategy name
     log: true,
-    reserveClassName: ['fa', 'fas', 'far', 'p', 'm', 'z', 'el', 'pt', 'pb', 'px', 'py', 'pl', 'pr', 'mt', 'mb', 'mx', 'my', 'ml', 'mr', 'to'],
+    reserveClassName: ['fa', 'fas', 'far','fab','fad', 'p', 'm', 'z', 'el', 'pt', 'pb', 'px', 'py', 'pl', 'pr', 'mt', 'mb', 'mx', 'my', 'ml', 'mr', 'to'],
+    classNameRegExp: '(bg|[-]*p[xylrbt]*|[-]*m[xylrbt]*|[-]*left|[-]*top|[-]*right|[-]*bottom|w|[-]*z|h|fa|fas|far|fab|fad|justify|overflow|border|max|flex|text|font|inline|rounded|from|to|via|contrast|brightness|leading|items|backdrop|shadow|duration|whitespace|self|cursor|transition|outline)-[a-z0-9_-]+|shadow|flex|rounded|border',
     ignorePrefixRegExp: '(.*tns.*|light[bB]ox|popover|el-|popper|is-|top-start|top-end|bottom-start|bottom-end|left-start|left-end|right-start|right-end)',
-    fileMatchRegExp: '.+\.js.*$|.+\.html.*$|.+\.vue.*$',
+    fileMatchRegExp: '.+\.js.*$|.+\.js.*$|.+\.html.*$|.+\.vue.*$',
     fileExlusionRegExp: '.+\.json.*$|.+\.geojson.*$',
   });
   webpackPlugins.push(myManglePlugin);
