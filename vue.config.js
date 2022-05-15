0fe2be5bf6ff8ad014935edbb284ffc7f8238087
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-05-15 17:56:42 +0200

    working

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,12 @@ fs.writeFile('./commit.json',
     if (err) return console.log(err);
   }
 );
+fs.writeFile('./CFDTrackJoiner/commit.json',
+  JSON.stringify(commit),
+  'utf8', function (err) {
+    if (err) return console.log(err);
+  }
+);
 var path = require('path');
 
 /*generate auth0-conf.json*/
