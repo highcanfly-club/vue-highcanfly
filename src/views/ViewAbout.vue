b6fdeafed3e0ac6bb985991db951fae7f3a35719
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-06-25 15:55:04 +0200

    no eslint exception

--- src/views/ViewAbout.vue
+++ src/views/ViewAbout.vue
@@ -224,7 +224,6 @@ import MainFooter from "@/components/Footers/MainFooter.vue";
 import { ref, defineComponent } from "vue";
 import logo from "@/assets/img/logo_high_can_fly.svg";
 import { getCloudinaryResponsiveBackground } from "@/plugins/highcanfly";
-//eslint-disable-next-line @typescript-eslint/no-var-requires
 import nodeVersion  from '@/config/node-version.json'
 const backgroundImage = "static-web-highcanfly/mountain";
 
