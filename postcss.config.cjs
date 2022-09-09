2c24cdc7e23504bcd1f66aa4c02b81e529201780
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-09-09 15:01:49 +0200

    safelisting classes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
         const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
         return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
       },
-      safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /tick.*/],
+      safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/, /tick.*/,/^dp/, /^cesium/, /^leaflet/,/^basicLightbox/, /^tns/, /^el-/, /^is-/, /popper/],
       fontFace: true
     }) : () => { },
   ]
