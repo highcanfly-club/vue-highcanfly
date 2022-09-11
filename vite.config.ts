784907b203d0f294f6a220fc3410059877cd35eb
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-09-11 20:04:34 +0200

    use fontawesome minify vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,16 @@ This website use:
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import cesium from './src/plugins/cesium';
+import cesium from "./src/plugins/cesium";
+import vitePluginFontawesomeminify from "./fontawesome";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cesium({addTags: false})],
+  plugins: [
+    vue(),
+    cesium({ addTags: false }),
+    vitePluginFontawesomeminify({ glyphWhitelist: ["?"] }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
