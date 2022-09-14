125ecf5784b44ea5efc4864fadf44173ff7b387e
Ronan LE MEILLAT <rtkgps@outlook.com>
2022-09-14 14:33:38 +0200

    BUG: only include fontawesome in minifation

--- fontawesome/index.ts
+++ fontawesome/index.ts
@@ -8,6 +8,7 @@ import { Plugin, UserConfig, ResolvedConfig } from 'vite';
 import colors from 'picocolors'
 
 const FA_FONT_REGEX = /fa-.*\.(eot|ttf|svg|woff|woff2)(\?.+)?$/
+const FA_TTF_FILTER = "fa-*.ttf"
 const TTF_REGEX = /fa-.*\.(ttf)(\?.+)?$/
 const TEXT_REGEX = /\.(js|css|html)$/
 const GLYPH_REGEX = /content\s*:[^};]*?('|")(.*?)\s*('|"|;)/g
@@ -135,6 +136,7 @@ interface VitePluginFontawesomeminifyOptions {
   outFontExtension?: string[];
   baseDir?: string;
   glyphWhitelist?: string[];
+  faTTFFontFilter?: string;
 }
 
 export default function vitePluginFontawesomeminify(options: VitePluginFontawesomeminifyOptions = {}): Plugin {
@@ -144,7 +146,8 @@ export default function vitePluginFontawesomeminify(options: VitePluginFontaweso
     outFontExtension = FONTMIN_EXTENSIONS,
     fontRegex = FA_FONT_REGEX,
     baseDir = BASE_DIR,
-    glyphWhitelist = GLYPH_WHITELIST
+    glyphWhitelist = GLYPH_WHITELIST,
+    faTTFFontFilter = FA_TTF_FILTER
   } = options;
 
   let config: ResolvedConfig
@@ -194,7 +197,7 @@ export default function vitePluginFontawesomeminify(options: VitePluginFontaweso
                 text: glyphsAndWhiteList,
                 hinting: true
               }))
-              .src(`${BASE_DIR}/*.ttf`)
+              .src(`${BASE_DIR}/${faTTFFontFilter}`)
               .dest(`${BASE_DIR}/`)
               //.use(Fontmin.ttf2eot())
               .use(Fontmin.ttf2woff({
